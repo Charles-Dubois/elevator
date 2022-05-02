db486d0c8f63d892b21085699dcd366fb3c3c6d5
Charles Dubois <charles.dubois.h@gmail.com>
2022-05-02 14:06:19 +0200

    app react created

---
+++ front-end/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import Elevator from "./views/Elevator";
+export default function App() {
+  return (
+    <>
+      <Elevator />
+    </>
+  );
+}
---
+++ front-end/src/views/Elevator.js
@@ -0,0 +1,30 @@
+export default function Elevator() {
+  return (
+    <section>
+      <div className="stageContainer">
+        <p className="stage">Voyant</p>
+        <p className="stage">Voyant</p>
+      </div>
+      <div className="elevatorButtonContainer">
+        <div className="elevatorButtonRow">
+          <button className="elevatorButton">Click</button>
+          <button className="elevatorButton">Click</button>
+          <button className="elevatorButton">Click</button>
+        </div>
+        <div className="elevatorButtonRow">
+          <button className="elevatorButton">Click</button>
+          <button className="elevatorButton">Click</button>
+          <button className="elevatorButton">Click</button>
+        </div>
+        <div className="elevatorButtonRow">
+          <button className="elevatorButton">Click</button>
+          <button className="elevatorButton">Click</button>
+          <button className="elevatorButton">Click</button>
+        </div>
+        <div className="elevatorButtonRow">
+          <button className="elevatorButton">Click</button>
+        </div>
+      </div>
+    </section>
+  );
+}
