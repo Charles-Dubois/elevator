d3510a26a8df14751a1758c5d9aa444bca5eca2a
Charles Dubois <charles.dubois.h@gmail.com>
2022-05-02 12:23:27 +0200

    server launched

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,22 @@
+require("dotenv").config();
 const express = require("express"),
   mongoose = require("mongoose"),
-  app = express();
+  app = express(),
+  { MONGODB_URI } = process.env,
+  PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(console.log("connected to mongo"))
+  .catch((err) => console.log(err));
+
+app.get("/", (_req, res) => {
+  res.send("hello world");
+});
+
+app.get("*", (_req, res) => {
+  res.status(404).send("Error 404");
+});
+app.listen(PORT, () => {
+  console.log("Listen on port " + PORT);
+});
