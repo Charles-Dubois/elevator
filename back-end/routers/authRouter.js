a8d7e78aa04132db478bdcff8cfef45214486012
Charles Dubois <charles.dubois.h@gmail.com>
2022-05-02 12:57:35 +0200

    route pour se déconnecter en supprimant un cookie

--- back-end/routers/authRouter.js
+++ back-end/routers/authRouter.js
@@ -31,11 +31,28 @@ router.post("/register", async (req, res) => {
   );
 
   //add token in cookie
-  res.cookie("justifytext", token, { httpOnly: true, secure: false });
+  res.cookie("elevator-api", token, { httpOnly: true, secure: false });
 
   res
     .status(201)
     .json({ message: "Request success", description: "Account created" });
+});
+
+router.delete("/logout", async (_req, res) => {
+  try {
+    res.clearCookie("elevator-api");
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: "Error 400 bad request",
+      description: "Cookie not found, check that you are connected",
+    });
+  }
+
+  res.json({
+    message: "Request success",
+    description: "You've been diconnected",
+  });
 });
 
 module.exports = router;
